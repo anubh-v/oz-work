b624da019a24c47688f966f51bf9455fe5a2900d
anubh-v <anubhav@u.nus.edu>
2020-12-21 20:10:34 +0800

    Update thread manager

--- threads-as-async-generators/thread.js
+++ threads-as-async-generators/thread.js
@@ -1,4 +1,4 @@
-class ThreadManager {
+export class ThreadManager {
     constructor() {
         this.sleepingThreads = [];
         this.currentThreadId = 0;
@@ -13,23 +13,36 @@ class ThreadManager {
     spawn(threadGenerator) {
         const threadId = this.nextId;
         this.nextId += 1;
-        this.sleepingThreads.push([threadId, threadGenerator]); 
+        this.sleepingThreads.push([threadId, false, threadGenerator({id: threadId})]); 
     }
 
-    run(threadId) {
+    spawnThreads() {
+        for (let threadGenerator of arguments) {
+            this.spawn(threadGenerator);
+        }
+    }
+
+    async run(threadId) {
         // remove thread from sleeping threads
-        const threadIndex = this.sleepingThreads.findIndex(([id, generator]) => id === threadId);
-        const [id, generator] = this.sleepingThreads.splice(threadIndex, 1)[0];
+        const threadIndex = this.sleepingThreads.findIndex(([id, hasRun, generator]) => id === threadId);
+        const [id, hasRun, generator] = this.sleepingThreads.splice(threadIndex, 1)[0];
         this.currentThreadId = id;
 
         // run thread
-        for await (let value of generator) {
-            // if thread asks to suspend
-            if (value == 'SUSPEND') {
+        let result = await generator.next();
+
+        while(true) {
+            if (result.done) {
+                break;
+            }
+
+            if (result.value == 'SUSPEND') {
                this.sleep(generator, id);
                break;
             }
+            result = await generator.next();
         }
+
 
         // if thread is completed / suspended, pick another sleeping thread to run (if any)
         if (this.sleepingThreads.length !== 0) {
@@ -39,11 +52,11 @@ class ThreadManager {
     }
 
     sleep(threadGenerator, threadId) {
-        this.sleepingThreads.push([threadId, threadGenerator]);
+        this.sleepingThreads.push([threadId, true, threadGenerator]);
     }
 
     selectAnotherThread(threadIdToAvoid) {
-        return this.sleepingThreads.find(([id, generator]) => id !== threadIdToAvoid)[0];
+        return this.sleepingThreads.find(([id, hasRun, generator]) => id !== threadIdToAvoid)[0];
     }
 
 }
