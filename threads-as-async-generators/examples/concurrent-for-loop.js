ca96123a4f16f09b147013f96cbf2c5427d6c6ba
anubh-v <anubhav@u.nus.edu>
2020-12-21 20:25:22 +0800

    Demonstrate delegation

--- threads-as-async-generators/examples/concurrent-for-loop.js
+++ threads-as-async-generators/examples/concurrent-for-loop.js
@@ -1,23 +1,30 @@
-import { ThreadManager } from './thread.js';
+import { ThreadManager } from '../thread.js';
 
 const manager = new ThreadManager();
 
-async function* dummyWork(threadState) {
-    yield 'SUSPEND';
+async function* foo(threadState) {
+    console.log(`running thread ${threadState.id}`);
+    yield* baz(threadState);
+    manager.spawnThreads(bar);
+}
 
-    console.log(`starting generator in thread ${threadState.id}`);
+async function* baz(threadState) {
     for (let i = 0; i < 100; i = i + 1) {
-        console.log(`thread ${threadState.id}`);
-        console.log(i);
+        console.log(`thread ${threadState.id} is at iteration ${i}`);
         if ((i % 10) === 0) {
-            console.log(i);
             yield 'SUSPEND';
         }
     }
 }
 
+async function* bar(threadState) {
+    console.log(`running thread ${threadState.id}`);
+    return 100; // not used
+}
+
 manager.start(async function*(threadState) {
-    manager.spawnThreads(dummyWork, dummyWork);
+    manager.spawnThreads(foo, foo, foo, foo);
+
 });
 
 
