135b51e8711d5899766679128da783977c0691e1
anubh-v <anubhav@u.nus.edu>
2021-01-06 23:24:36 +0800

    Add third threads' implementation

---
+++ threads-as-async-generators-with-explicit-stack/thread.js
@@ -0,0 +1,196 @@
+import { Message } from './message.js';
+import { performance } from 'perf_hooks';
+ 
+export class ThreadManager {
+    constructor() {
+        this.runnableThreads = [];
+        this.blockedThreads = []; // threads waiting for promise-like objects to be resolved
+        this.currentThreadId = 0;
+        this.nextId = 0;
+        this.numSwitches = 0; // track number of thread switches, for debugging purposes
+    }
+
+    start(firstThreadGenerator) {
+      this.spawn(firstThreadGenerator);
+      this.startTime = performance.now();
+      this.run(firstThreadGenerator);
+    }
+
+    spawn(threadGenerator) {
+        const threadId = this.nextId;
+        this.nextId += 1;
+        const threadState = {id: threadId, startTime: performance.now()};
+        this.runnableThreads.push([threadId, [threadGenerator(threadState)], undefined]); 
+    }
+
+    spawnThreads() {
+        for (let threadGenerator of arguments) {
+            this.spawn(threadGenerator);
+        }
+    }
+
+    async run(threadId) {
+        // remove thread from sleeping threads
+        const threadIndex = this.runnableThreads.findIndex(([id, stack, arg]) => id === threadId);
+        let [id, stack, arg] = this.runnableThreads.splice(threadIndex, 1)[0];
+        this.currentThreadId = id;
+        let currentFrame = stack.pop()
+
+        // run thread
+        while(true) {
+            let result;
+
+            if (arg != undefined) {
+                result = await currentFrame.next(arg);
+            } else {
+                result = await currentFrame.next();
+            }
+
+            if (result.done) {
+                arg = result.value;
+                currentFrame = stack.pop();
+                break;
+            }
+
+            if (result.value instanceof Message) {
+               if (result.value.isSuspend()) {
+                   this.suspend(generator, id);
+                   break;
+               }
+
+               if (result.value.isPromiseLike()) {
+                   // indicate that current thread is blocked
+                   this.block(generator, id);
+                   Promise.resolve(result.value.value).then((arg) => {
+                       this.runnableThreads.push([id, generator, arg]);
+                       this.unblock(id);
+                   });
+                   break;
+               }
+
+               if (result.value.isCall()) {
+                   stack.push(currentFrame);
+                   currentFrame = result.value.value;
+               }
+            }
+
+            arg = undefined;
+        }
+
+        // TODO: schedule a timeout if needed
+        if (this.hasExceededRunningTime()) {
+            console.log('timing out');
+            setTimeout(() => {
+                this.startTime = performance.now();
+                this.poll();
+            }, 0);
+            return;
+        }
+
+        // if thread is completed / suspended / blocked, pick another runnable thread to run (if any)
+        if (this.runnableThreads.length !== 0) {
+            const nextThreadToRun = this.selectThreadToRun(this.currentThreadId);
+            this.run(nextThreadToRun);
+            return;
+        }
+
+        if (this.blockedThreads.length !== 0) {
+            // no more runnable threads, but there are blocked threads
+            // schedule a poll after some time
+            setTimeout(() => this.poll(), 0);
+        }
+        
+    }
+
+    suspend(stack, threadId) {
+        this.numSwitches += 1;
+        this.runnableThreads.push([threadId, stack, undefined]);
+    }
+
+    // Add a thread into the list of blocked threads
+    block(stack, threadId) {
+        this.numSwitches += 1;
+        this.blockedThreads.push([threadId, stack]);
+    }
+
+    // Remove a thread from the list of blocked threads
+    unblock(threadId) {
+        const indexToRemove = this.blockedThreads.findIndex(([id, stack]) => id === threadId);
+        this.blockedThreads.splice(indexToRemove, 1);
+    }
+
+    /*
+     Precondition: At least one runnable thread is present
+     */
+    selectThreadToRun(previousThread) {
+        if (this.runnableThreads.length === 1) {
+            return previousThread; // there is only one runnable thread, just continue it
+        } else {
+            return this.runnableThreads.find(([id, stack, arg]) => id !== previousThread)[0];
+        }
+    }
+
+    poll() {
+        // check if there are any runnable threads
+        // if yes, run one of them
+        if (this.runnableThreads.length !== 0) {
+            const nextThreadToRun = this.runnableThreads[0][0];
+            this.run(nextThreadToRun);
+            return;
+        }
+
+        // if no runnable threads, check if there are blocked threads
+        if (this.blockedThreads.length !== 0) {
+            // schedule another poll after some time
+            setTimeout(() => this.poll(), 0);
+            return;
+        }
+
+        // if no runnable, and no blocked threads, no further work needed by ThreadManager
+        
+    }
+
+    hasExceededRunningTime() {
+        const timeNow = performance.now();
+        return (timeNow - this.startTime) > 500;
+    }
+
+    shouldSuspend(threadState) {
+        const timeNow = performance.now() - threadState.startTime;
+        return timeNow > 20;
+    }
+
+    async *suspendAndCall(...args) {
+        const threadState = args[0];
+        const obj = args[1];
+        const func = args[2];
+        args.splice(0, 3);
+        const funcArgs = args;
+        if (this.shouldSuspend(threadState)) {
+            yield new Message('SUSPEND');
+        }
+
+        threadState.startTime = performance.now();
+
+        if (func.isInternal) {
+            if (obj === undefined) {
+                yield new Message('CALL', func(threadState, ...funcArgs));
+            } else {
+                yield new Message('CALL', func.call(obj, ...funcArgs));
+            }
+        } else {
+            if (obj === undefined) {
+               return func(...funcArgs);
+            } else {
+                return func.call(obj, ...funcArgs);
+            }
+        }
+    }
+
+
+}
+
+export function mark(func) {
+    func.isInternal = true;
+    return func;
+}
---
+++ threads-as-async-generators-with-explicit-stack/transpiler.js
@@ -0,0 +1,129 @@
+import { default as falafel } from 'falafel';
+import { default as fs } from 'fs';
+
+const threadPreamble = `import { ThreadManager, mark } from "./thread.js";
+                        import { Message } from "./message.js";
+                        const manager = new ThreadManager();
+                        const thread = function(...args) {
+                            manager.spawnThreads(...args);
+                        }`;
+
+const infile = process.argv[2];
+const outfile = process.argv[3];
+
+fs.readFile(infile, 'utf8', function(err, data) {
+  const transpiledCode = transform(data);
+  fs.writeFile(outfile, transpiledCode, 'utf8', function(err) {
+    if (err) console.log(`Error writing transpiled code to file: ${err}`);
+  });
+});
+
+function transform(source) {
+  let output = falafel(source, function (node) {
+    if (node.type === 'FunctionDeclaration') {
+      transformFunctionDefinition(node);
+    }
+
+    if (node.type === 'FunctionExpression') {
+      transformFunctionExpression(node);
+    }
+
+    if (node.type === 'CallExpression') {
+      transformCalls(node);
+    }
+
+    if (node.type === 'ArrowFunctionExpression') {
+      transformArrowFunction(node);
+    }
+
+    if (node.type === 'AwaitExpression') {
+      transformAwaitExpression(node);
+    }
+  });
+
+  return wrapTopLevel(output);
+}
+
+/**
+ * Converts a function definition into a generator function definition.
+ * The 1st argument of the generator function will be an object containing the thread state.
+ */
+function transformFunctionDefinition(node) {
+  const functionId = node.id.source();
+  let transformedCode = `async function* ${functionId} (${insertThreadStateIntoArgs(node.params)})
+    ${node.body.source()}`;
+  
+  transformedCode += `mark(${functionId});`
+  node.update(transformedCode);
+}
+
+/**
+ * Converts a function expression into a generator function expression.
+ * The 1st argument of the generator function will be an object containing the thread state.
+ */
+function transformFunctionExpression(node) {
+  const transformedCode = `mark(async function* (${insertThreadStateIntoArgs(node.params)})
+    ${node.body.source()})`;
+  
+  node.update(transformedCode);
+}
+
+/**
+ * Converts an arrow function expression into a generator function expression.
+ * The 1st argument of the generator function will be an object containing the thread state.
+ */
+function transformArrowFunction(node) {
+  const transformedCode = `mark(async function*(${insertThreadStateIntoArgs(node.params)}) { ${node.body.source()} })`;
+  node.update(transformedCode);
+}
+
+function transformCalls(node) {
+
+  if (isThreadCall(node)) {
+    node.arguments.forEach(arg => arg.source());
+    return;
+  }
+
+  if (node.callee.type === 'MemberExpression') {
+    return transformMethodCalls(node);
+  }
+  
+  const transformedCode = `(yield* manager.suspendAndCall(threadState, undefined, ${node.callee.source()}, ${getArgs(node.arguments).reduce((a, c) => a + ',' + c)}))`;
+  node.update(transformedCode);
+}
+
+function transformMethodCalls(node) {
+  const obj = node.callee.object.source();
+  const func = node.callee.source();
+
+  const transformedCode = `(yield* manager.suspendAndCall(threadState, ${obj}, ${func}, ${getArgs(node.arguments).reduce((a, c) => a + ',' + c)}))`;
+  node.update(transformedCode);
+}
+
+function transformAwaitExpression(node) {
+  const promise = node.argument;
+  const transformedCode = `(yield new Message('AWAIT', ${promise.source()}))`;
+  node.update(transformedCode);
+}
+
+
+function insertThreadStateIntoArgs(argsArray) {
+  return argsArray.reduce((a, c) => a + ',' + c.source(), 'threadState');
+}
+
+function getArgs(argsArray) {
+  return argsArray.map(arg => arg.source());
+}
+
+function isThreadCall(node) {
+  return node.callee.type === 'Identifier' && node.callee.name === 'thread';
+}
+
+function wrapTopLevel(topLevelSource) {
+  return ` ${threadPreamble}
+  manager.start(async function*(threadState) {
+    ${topLevelSource}
+  }); `;
+}
+
+
